c5e94b7d91244cc304359486da2010e34bde792a
Meet Patel <71483201+Meetpatel-windsor@users.noreply.github.com>
2020-11-14 16:17:22 +0530

    Add files via upload
    This upload contains the code of sin function

--- TrignometicFunctions/src/Trignometic.java
+++ TrignometicFunctions/src/Trignometic.java
@@ -44,7 +44,26 @@ public class Trignometic {
 		
 		 public static double sine2(double x){
 			 
-			 return 0;
+			 int status=0;
+			 if(x<0) {
+				 x=x*-1;
+				 status=1;
+			 }
+			 
+			 double sin=x,term, numerator=x, denominator=1, 
+		        		xsquare=x*x, factorial=1, sign=-1;
+		        do{
+		            numerator *= xsquare;
+		            denominator=denominator*(factorial+1)*(factorial+2);
+		            factorial=factorial+2;
+		            term=numerator/denominator;
+		            sin=sin+(sign*term);
+		            sign*=-1;
+		        }
+		        while(term>0.00);
+		        
+		        return status==1?-1*sin:sin;
+			 
 			 
 		 }
 		 
@@ -92,6 +111,7 @@ public class Trignometic {
 	    public static void main(String args[]){
 	    	
 	    	System.out.println(Math.cos(0.7853981633974483)+" "+cos(0.7853981633974483));
+	    	System.out.println(Math.sin(0.7853981633974483)+" "+sine2(0.7853981633974483));
 
 	        
 	    }
